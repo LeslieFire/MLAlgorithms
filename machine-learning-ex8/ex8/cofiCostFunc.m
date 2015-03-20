1cf46d3bc4104587600ca3672a246a20a8e66c64
Leslie <249893977@qq.com>
2015-03-20 09:58:28 +0800

    Collaborative Filtering

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,13 +40,24 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
+M = (X * Theta' - Y) .^ 2;
 
+J = 1/2 * sum(sum( R .* M)) + lambda/2 * sum(sum( Theta.^2 )) + lambda/2 * sum(sum( X.^2 ));
+     
+for i = 1:num_movies
+    idx = find(R(i, :) == 1);
+    Thetatemp = Theta(idx, :);
+     Ytemp = Y(i, idx);
+     X_grad(i, :) = (X(i, :) * Thetatemp' - Ytemp) * Thetatemp + lambda * X(i , :);
+end
 
+for j = 1:num_users
+    idx = find(R(:, j) == 1);
+    Xtemp = X(idx, :);
+    Ytemp = Y(idx, j);
+    Theta_grad(j, :) = (Xtemp * Theta(j, :)' - Ytemp)' * Xtemp + lambda * Theta(j, : );
 
-
-
-
-
+end
 
 
 
